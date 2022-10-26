b9fa170193ac1cdcec3cea7c5bb62aa2ca26a95f
Daiki <s21C1043OE@s.chibakoudai.jp>
2022-10-26 20:30:19 +0900

    さらなる改良

--- test.bash
+++ test.bash
@@ -1,7 +1,18 @@
 #!/bin/bash
+#SPDX-FileCopyrightText: 2022 Daiki Kurokawa
+#SPDX-Licencse=Identifier: BSD-3-Clayse
+ng () {
+      echo NG at line $1
+      res=1
+}
 
+res=0
+
+###I/O TEST ###
 out=$(seq 5 | ./plus)
+[ "${out}" = 14.0 ] || ng ${LINENO}
 
-[ "${out}" = 15 ]
+[ "res" = 0 ] && echo OK
+exit $res
 
 
