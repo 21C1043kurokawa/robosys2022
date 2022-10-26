68520a85e4aac2e00cf03cb8ddf9315d37682cb2
Daiki <s21C1043OE@s.chibakoudai.jp>
2022-10-26 20:46:23 +0900

    シェルスクリプトの挙動の確認

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 #SPDX-FileCopyrightText: 2022 Daiki Kurokawa
 #SPDX-Licencse=Identifier: BSD-3-Clayse
 ng () {
@@ -12,7 +12,16 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
-[ "res" = 0 ] && echo OK
-exit $res
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]   || ng${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+out=$(echo | ./plus)
+[ "$?" = 1　] || ng ${LINENO}
+[ "$(out)" = ""] ||ng ${LINENO}
+
+[ "$res" = 0 ] && echo OK
+  exit $res
 
 
