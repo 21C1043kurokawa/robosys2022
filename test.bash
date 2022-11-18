9b16193f0a7eca9806ae5c1e5d73f421d4aca5a3
Daiki <s21C1043OE@s.chibakoudai.jp>
2022-11-19 07:52:11 +0900

    practice1120

--- test.bash
+++ test.bash
@@ -1,18 +1,30 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2022 Daiki Kurokawa
 # SPDX-License-Identifier: BSD-3-Clause
 
-ng () {
-      echo NG at Line $1
-      res=1
-}
+  ng () {
+        echo NG at Line $1
+        res=1
+  }
 
-res=0
+  res=0
 
-###1/0 test ###
-out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
+  ###1/0 test ###
+  out=$(seq 5 | ./plus)
+  [ "${out}" = 15 ] || ng ${LINENO}
 
-[ "$res" = 0 ] && echo OK
-exit $res
+  [ "$res" = 0 ] && echo OK
+  exit $res
 
+  ###strange input ###
+  out=$(echo あ | ./plus)
+  [ "$?" = 1 ]       || ng ${LINENO}
+  [ "{out}" = "" ]   || ng ${LINENO}
+
+  out=$(echo | ./plus) ###空文字
+  [ "$" = 1 ]        || ng ${LINENO}
+  [ "${out}" = "" ]  || ng ${LINENO}
+
+
+[ "$res = 0 ] && echo OK
+  exit $res
