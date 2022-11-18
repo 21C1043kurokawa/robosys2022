0b50c033e251eaeca1f92686e59b98871fdfb463
Daiki <s21C1043OE@s.chibakoudai.jp>
2022-11-19 06:04:56 +0900

    practice

--- test.bash
+++ test.bash
@@ -1,27 +1,29 @@
-#!/bin/bash -xv
-#SPDX-FileCopyrightText: 2022 Daiki Kurokawa
-#SPDX-Licencse=Identifier: BSD-3-Clayse
+#!/bin/bash
 ng () {
-      echo NG at line $1
+      echo NG at Line $1
       res=1
 }
-
+　
 res=0
-
-###I/O TEST ###
+　
+### I/O TEST ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 15 ] || ng ${LINENO}
-
-### STRANGE INPUT ###
-out=$(echo あ | ./plus)
-[ "$?" = 1 ]   || ng${LINENO}
-[ "${out}" = "" ] || ng ${LINENO}
-
-out=$(echo | ./plus)
-[ "$?" = 1　] || ng ${LINENO}
-[ "${out}" = ""] ||ng ${LINENO}
-
+[ "${out}" = 14 ] || ng ${LINENO}
+　
+[ "$res" = 0 ] && echo OK        # &&（AND記号）は左側が成功すると右側を実行
+exit $res
+### I/O ###
+  out=$(seq 5 | ./plus)
+  [ "${out}" = 15 ] || ng ${LINENO}
+    　 
+  ### STRANGE INPUT ###
+  out=$(echo あ | ./plus)
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+    　 
+  out=$(echo | ./plus) #空文字
+  [ "$?" = 1 ]      || ng ${LINENO}
+  [ "${out}" = "" ] || ng ${LINENO}
+    　 
 [ "$res" = 0 ] && echo OK
   exit $res
-
-
